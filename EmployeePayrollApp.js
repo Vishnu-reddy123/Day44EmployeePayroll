12c1540bf67833653933f14260b40a21cbe48aa5
Vishnu-reddy123 <vishnu95532@gmail.com>
2023-01-02 16:04:32 +0530

    added logic to set event listner

---
+++ EmployeePayrollApp.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded',(event) =>{
+    const text=document.querySelector('#name');
+            const textError=document.querySelector('.text-error');
+            text.addEventListener('input',function(){
+                if(text.value.length == 0){
+                    textError.textContent="";
+                    return;
+                }try{
+                    (new EmployeePayrollData()).name=text.value;;
+                    textError.textContent="";
+                }catch (e){
+                    textError.textContent=e;
+                }
+            });
+    const salary=document.querySelector('#salary');
+    const output=document.querySelector('.salary-output');
+    output.textContent=salary.value;
+    salary.addEventListener('input',function(){
+        output.textContent=salary.value;
+    });
+    });
